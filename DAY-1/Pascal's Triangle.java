ca14355ba788e563ec6c6cc3495a282dceb338d6
Lavisha Soni <67314839+coder88-lang@users.noreply.github.com>
2023-06-06 12:45:55 +0530

    Update Pascal's Triangle.java

--- DAY-1/Pascal's Triangle.java
+++ DAY-1/Pascal's Triangle.java
@@ -1,1 +1,28 @@
 
+import java.io.*;
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class Solution {
+	public static ArrayList<ArrayList<Long>> printPascal(int n) {
+			ArrayList<ArrayList<Long>> ans = new ArrayList<>();
+
+         for (long row = 1; row <= n; row++) {
+            ans.add(generateRow(row));
+        }
+        return ans;
+    }
+
+    public static ArrayList<Long> generateRow(Long numRows) { 
+        long ans = 1;
+        ArrayList<Long> ls = new ArrayList<>();
+        ls.add((long)1);
+        for (long i = 1; i < numRows; i++) {
+            ans = ans * (numRows-i);
+            ans  = ans/i;
+            ls.add(ans);
+        }
+        return ls;
+	}
+}
